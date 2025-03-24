76d1a7e0f2e440e62d04d8bf44c0e001af31e5d4
Julie <julie.trucvallet@gmail.com>
2025-03-24 15:59:12 +0100

    updated

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,6 @@ import { Router } from 'express'
 import { createUser, loginUser } from '../controllers/authController.js'
 
 const authRouter = Router()
-const JWT_SECRET = process.env.JWT_SECRET
 
 authRouter.post(`/register`, createUser)
 
