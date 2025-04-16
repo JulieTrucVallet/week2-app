406893796a988e94d0eab6e87e721647bf73d06e
Julie <julie.trucvallet@gmail.com>
2025-04-16 12:38:10 +0200

    Updated

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
-import { getUserProfile } from '../controllers/usersController'
+import { getAllUsers, getUserByID, getUserProfile } from '../controllers/usersController'
 import { verifyUser } from '../middlewares/verifyUser'
 
 const userRouter = Router()
 
 userRouter.get('/profile', verifyUser, getUserProfile)
+
+userRouter.get('/users', getAllUsers)
+userRouter.get('/users/:id', getUserByID)
 
 export default userRouter
